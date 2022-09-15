be954997e40ec2680c1bea895aef017d5af0a8e3
kim-ju-ho <kim1jo2ho3@naver.com>
2022-09-15 22:50:37 +0900

    first commit

---
+++ src/main/java/com/example/ooppractice/calculator/Calculator.java
@@ -0,0 +1,10 @@
+package com.example.ooppractice.calculator;
+
+
+public class Calculator {
+
+    public static int calculator(int operand1, String operator,int operand2) {
+
+      return ArithmeticOperator.calculator(operand1,operator,operand2);
+    }
+}
---
+++ src/test/java/com/example/ooppractice/UserTest.java
@@ -0,0 +1,35 @@
+package com.example.ooppractice;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+class UserTest {
+
+    @DisplayName("패스워드 초기화 여부를 판단하다.")
+    @Test
+    void passwordTest(){
+        // given
+        User user = new User();
+        // when
+        // 구현체를 만들어서 주입할 필요없이 람다를 이용해서 사용할 수도 있음
+        // interface를 이용해서 결합도를 낮추웠기때문
+        user.initPassword(()->"abcdefg");
+        // then
+        assertThat(user.getPassword()).isNotNull();
+    }
+
+    @DisplayName("패스워드가 요구사항에 부합되지 않아 초기화가 되지 않는다")
+    @Test
+    void passwordTest2(){
+        // given
+        User user = new User();
+        // when
+        user.initPassword(new WrongFixedPasswordGenerator());
+        // then
+        assertThat(user.getPassword()).isNull();
+    }
+
+}
