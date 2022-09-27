bff867733e8ab2e79203ce32aeee9d06dced0ad2
kim-ju-ho <kim1jo2ho3@naver.com>
2022-09-28 00:07:56 +0900

    feat. was-practice

---
+++ was-practice/src/main/groovy/com/example/waspractice/WasPracticeApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.waspractice
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class WasPracticeApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(WasPracticeApplication, args)
+    }
+
+}
