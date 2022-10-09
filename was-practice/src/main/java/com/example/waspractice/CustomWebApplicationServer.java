068e51ca479c94747da79b6290d502df6feddadd
kim-ju-ho <kim1jo2ho3@naver.com>
2022-10-09 12:23:23 +0900

    thread pool

--- was-practice/src/main/java/com/example/waspractice/CustomWebApplicationServer.java
+++ was-practice/src/main/java/com/example/waspractice/CustomWebApplicationServer.java
@@ -6,9 +6,14 @@ import org.slf4j.LoggerFactory;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CustomWebApplicationServer {
     private final int port;
+
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
 
     private static final Logger logger = LoggerFactory.getLogger(CustomWebApplicationServer.class);
 
@@ -25,8 +30,11 @@ public class CustomWebApplicationServer {
 
             while ((clientSocket = serverSocket.accept()) != null) {
                 logger.info("[CustomWebApplicationServer] client connected");
+                // - client의 요청이 올때마다 Thread를 하나씩 생성한다.
+                // new Thread(new ClientRequestHandler(clientSocket)).start();
 
-             new Thread(new ClientRequestHandler(clientSocket)).start();
+                // - ThreadPool을 이용해 사용할 Thread를 만들어 놓는다.
+                executorService.execute(new ClientRequestHandler(clientSocket));
             }
         }
     }
